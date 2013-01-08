643aa08adbb593a2c21870a58e211fcf66ece0a7
nbleisch <nbleisch@lichtflut.de>
2013-01-08 10:12:42 +0100

    Initial project setup

---
+++ frontend/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Frontend::Application.config.secret_token = '62851dc3361fb397e7a174e62c1e16d787825254dff494b87b103aa971ba1a84dbba9667daeb83e9da8e80a1ad7b90181ee04e9abfec7cf9c88ae9f5237f760a'
